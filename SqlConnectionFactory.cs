6ccdc79c6ec9984a8e1f18fc9253c6a666322065
Yamini <168359174+yaminipenugonda@users.noreply.github.com>
2024-12-06 11:47:03 +0530

    Add files via upload

---
+++ SqlConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ECommerceAPI.Data
+{
+    public class SqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection CreateConnection()
+            => new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
+
