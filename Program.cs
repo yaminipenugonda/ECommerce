4a5ec2e2fcb52234a03f10691d04f71bd259c8fa
Yamini <168359174+yaminipenugonda@users.noreply.github.com>
2024-12-06 11:49:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+
+using ECommerceAPI.Data;
+
+namespace ECommerceAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddTransient<SqlConnectionFactory>();
+            // Registering the Repositories
+            builder.Services.AddScoped<CustomerRepository>();
+            builder.Services.AddScoped<ProductRepository>();
+            builder.Services.AddScoped<OrderRepository>();
+            builder.Services.AddScoped<PaymentRepository>();
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
